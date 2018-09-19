ef4dde3ce05b803fa7f97e58f98d7eaf671feb0d
Michiel <linearza@users.noreply.github.com>
2018-09-19 23:28:47 +0200

    Adds alternative star background effect and tweaks colors and such a bit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,25 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+
+    // sassOptions: {
+    //   includePaths: [
+    //     'app'
+    //   ]
+    // },
+
+    // outputPaths: {
+    //   app: {
+    //     css: {
+    //       'app': '/assets/app.css',
+    //       // 'fonts': '/assets/fonts.css'
+    //     }
+    //   }
+    // }
+
   });
+
+
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
